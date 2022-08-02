9b5cc04ae13dbc66d88d85b94c56c19692078a5b
Kevin Suhajda <kevin@nevercode.io>
2022-08-02 08:56:08 +0200

    delete build folder

--- ios/build/ios_integ/Build/Products/Build/Intermediates.noindex/Runner.build/Release-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kevin/Documents/projects/inegrate/ios/build/ios_integ/Build/Products/Build/Intermediates.noindex/Runner.build/Release-iphoneos/Runner.build/DerivedSources/Runner_vers.c
