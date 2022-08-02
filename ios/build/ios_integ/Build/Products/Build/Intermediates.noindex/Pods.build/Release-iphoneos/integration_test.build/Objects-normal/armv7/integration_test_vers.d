1281da6cb5ecb9ba406c6e3ffe02e5037026bb96
Kevin Suhajda <kevin@nevercode.io>
2022-08-02 09:38:34 +0200

    delete builds

--- ios/build/ios_integ/Build/Products/Build/Intermediates.noindex/Pods.build/Release-iphoneos/integration_test.build/Objects-normal/armv7/integration_test_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kevin/Documents/projects/inegrate/ios/build/ios_integ/Build/Products/Build/Intermediates.noindex/Pods.build/Release-iphoneos/integration_test.build/DerivedSources/integration_test_vers.c
