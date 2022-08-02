a3de50cdca42c3e4675e2197dfec99ac3b01f934
Kevin Suhajda <kevin@nevercode.io>
2022-08-02 15:27:44 +0200

    remove second test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:inegrate/main.dart';
-import 'dart:io';
+// import 'dart:io';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
