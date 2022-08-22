7221d61853d20a80f9d65f53d1a27e5ca8318002
Kevin Suhajda <kevin@nevercode.io>
2022-08-22 19:52:51 +0200

    remove build files

--- ios/build/ios_integ/Build/Intermediates.noindex/Pods.build/Release-iphoneos/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/kevin/Documents/projects/inegrate/ios/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/kevin/Documents/projects/inegrate/ios/build/ios_integ/Build/Intermediates.noindex/Pods.build/Release-iphoneos/Pods-Runner.build/module.modulemap
--- ios/build/ios_integ/Build/Intermediates.noindex/Runner.build/Release-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kevin/Documents/projects/inegrate/ios/build/ios_integ/Build/Intermediates.noindex/Runner.build/Release-iphoneos/Runner.build/DerivedSources/Runner_vers.c
