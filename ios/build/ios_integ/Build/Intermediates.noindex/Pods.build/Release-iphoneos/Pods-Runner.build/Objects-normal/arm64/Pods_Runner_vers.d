8d1b4873f6e6befacdbe55d3a76937ec84c9327e
Kevin Suhajda <kevin@nevercode.io>
2022-08-22 19:51:18 +0200

    Xcode version

---
+++ ios/build/ios_integ/Build/Intermediates.noindex/Pods.build/Release-iphoneos/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/kevin/Documents/projects/inegrate/ios/build/ios_integ/Build/Intermediates.noindex/Pods.build/Release-iphoneos/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
---
+++ ios/build/ios_integ/Build/Intermediates.noindex/Pods.build/Release-iphoneos/integration_test.build/Objects-normal/arm64/integration_test-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/kevin/Documents/projects/inegrate/ios/Pods/Target\ Support\ Files/integration_test/integration_test-dummy.m \
+  /Users/kevin/Documents/projects/inegrate/ios/build/ios_integ/Build/Intermediates.noindex/Pods.build/Release-iphoneos/integration_test.build/module.modulemap \
+  /Users/kevin/Documents/projects/inegrate/ios/Pods/Target\ Support\ Files/integration_test/integration_test-prefix.pch
