826d3a939e9c35527898761522a04baa0c207a10
Nihal Agazade <43378371+icarusdust@users.noreply.github.com>
2022-09-08 17:27:25 +0300

    Update app_test.dart

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,40 +1,74 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:integration_test/integration_test.dart';
+// import 'package:inegrate/main.dart';
+// // import 'dart:io';
+
+// void main() {
+//   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+//   testWidgets('No input in name field displays error validation text',
+//       (WidgetTester tester) async {
+//     await tester.pumpWidget(MyApp());
+
+//     await tester.tap(find.byType(FloatingActionButton));
+//     await tester.pumpAndSettle();
+
+//     expect(find.byType(MyHomePage), findsOneWidget);
+//     expect(find.text('Input some text!'), findsOneWidget);
+//   });
+
+//   // testWidgets(
+//   //   'Text is entered into field and Alert is shown inclucing name entered',
+//   //   (WidgetTester tester) async {
+//   //     await tester.pumpWidget(MyApp());
+
+//   //     final inputText = 'Kevin';
+//   //     await tester.enterText(find.byKey(Key('text-field')), inputText);
+
+//   //     await tester.tap(find.byType(FloatingActionButton));
+//   //     await tester.pumpAndSettle();
+
+//   //     expect(find.text('Hey, Kevin!'), findsOneWidget);
+
+//   //     sleep(Duration(seconds: 5));
+
+//   //     await tester.tap(find.byType(TextButton));
+//   //     await tester.pumpAndSettle();
+//   //   },
+//   // );
+// }
+
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:inegrate/main.dart';
-// import 'dart:io';
+
+import 'package:flutter_integrations_test_example/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('No input in name field displays error validation text',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter',
+        (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
 
-    await tester.tap(find.byType(FloatingActionButton));
-    await tester.pumpAndSettle();
+      // Verify the counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
 
-    expect(find.byType(MyHomePage), findsOneWidget);
-    expect(find.text('Input some text!'), findsOneWidget);
+      // Finds the floating action button to tap on.
+      final Finder fab = find.byTooltip('Increment');
+
+      // Emulate a tap on the floating action button.
+      await tester.tap(fab);
+
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+
+      // Verify the counter increments by 1.
+      expect(find.text('1'), findsOneWidget);
+    });
   });
-
-  // testWidgets(
-  //   'Text is entered into field and Alert is shown inclucing name entered',
-  //   (WidgetTester tester) async {
-  //     await tester.pumpWidget(MyApp());
-
-  //     final inputText = 'Kevin';
-  //     await tester.enterText(find.byKey(Key('text-field')), inputText);
-
-  //     await tester.tap(find.byType(FloatingActionButton));
-  //     await tester.pumpAndSettle();
-
-  //     expect(find.text('Hey, Kevin!'), findsOneWidget);
-
-  //     sleep(Duration(seconds: 5));
-
-  //     await tester.tap(find.byType(TextButton));
-  //     await tester.pumpAndSettle();
-  //   },
-  // );
 }
+
